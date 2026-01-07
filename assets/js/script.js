d6c388377eae6281c08c2504e0c58d2cffe78627
paul philps <paulphilps@email.com>
2026-01-07 19:36:57 +0530

    [first commit]

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+new Typed("#typed", {
+  strings: ["Aman Khan", "Front-End Developer", "Web Designer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+  
+});
+
+// new Typed("#typed-sub", {
+//   strings: ["Front-End Developer", "Web Designer"],
+//   typeSpeed: 100,
+//   backSpeed: 60,
+//   loop: true
+// });
+
+
+const eduBars = document.querySelectorAll(".progress-fill");
+
+const eduObserver = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const value = entry.target.dataset.progress;
+        entry.target.style.width = value + "%";
+      }
+    });
+  },
+  { threshold: 0.6 }
+);
+
+eduBars.forEach(bar => eduObserver.observe(bar));
+
+const bars = document.querySelectorAll(".level-progress");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.width =
+          entry.target.dataset.level + "%";
+      }
+    });
+  },
+  { threshold: 0.6 }
+);
+
+bars.forEach(bar => observer.observe(bar));
+
+
+document.getElementById("emailLink").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const email = "digitalorbitals7819@gmail.com";
+  const subject = "Project Inquiry";
+
+  const isMobile =
+    /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    // MOBILE → default email app
+    window.location.href =
+      `mailto:${email}?subject=${encodeURIComponent(subject)}`;
+  } else {
+    // DESKTOP → Gmail web
+    window.open(
+      `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${encodeURIComponent(subject)}`,
+      "_blank"
+    );
+  }
+});
+
+
+
